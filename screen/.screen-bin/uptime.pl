1f7b7bb2ae2e3260600b5987bd8ac5ed4e8b3607
Yorokobi <colbyw@gmail.com>
2012-03-23 10:05:10 -0700

    Cosmetic changes to uptime perl script

--- screen/.screen-bin/uptime.pl
+++ screen/.screen-bin/uptime.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl 
+#!/usr/bin/env perl 
 #===============================================================================
 #
 #         FILE:  uptime.pl
@@ -13,9 +13,9 @@
 #        NOTES:  ---
 #       AUTHOR:  Yorokobi
 #      COMPANY:  ---
-#      VERSION:  1.1.1
+#      VERSION:  1.1.2
 #      CREATED:  2010-04-26 19:42:02 MST (UTC-7)
-#     REVISION:  2012-03-23 09:33:00 MST (UTC-7)
+#     REVISION:  2012-03-23 10:04:00 MST (UTC-7)
 #===============================================================================
 
 use strict;
@@ -42,18 +42,17 @@ eval { $minutes = int($uptime / $MIN  % $MIN); }; die $@ if $@;
 eval { $hours   = int($uptime / $HOUR % $DAY); }; die $@ if $@;
 eval { $days    = int($uptime / $HOUR / $DAY); }; die $@ if $@;
 
-if ($hours < 10) { $hours = "0$hours"; }
+### Do we want zero-padded numbers <10?
+#if ($hours < 10) { $hours = "0$hours"; }
+#if ($minutes < 10) { $minutes = "0$minutes"; }
+#if ($days < 10) { $days = "0$days"; }
 
 if ( $days == 0 ) {
     if ( $hours == 0 ) {
         print "$minutes mins";
     } else {
-        if ($minutes < 10) { $minutes = "0$minutes"; }
-        print "$hours:$minutes";
+        print "$hours"."h "."$minutes"."m";
     }
-    if ($minutes < 10) { $minutes = "0$minutes"; }
-} elsif ( $days == 1 ) {
-    print "$days day, $hours:$minutes";
 } else {
-    print "$days days, $hours:$minutes";
+    print "$days"."d "."$hours"."h "."$minutes"."m";
 }
