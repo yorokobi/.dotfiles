5ef48da67b636f19faad524e3dad0b45d4479ebd
Yorokobi <colbyw@gmail.com>
2012-03-23 09:33:46 -0700

    Added option to obtain uptime in seconds from *BSD

--- screen/.screen-bin/uptime.pl
+++ screen/.screen-bin/uptime.pl
@@ -13,9 +13,9 @@
 #        NOTES:  ---
 #       AUTHOR:  Yorokobi
 #      COMPANY:  ---
-#      VERSION:  1.1
-#      CREATED:  2010-04-26 19:42:02 MST
-#     REVISION:  2010-04-28 09:40:00 MST
+#      VERSION:  1.1.1
+#      CREATED:  2010-04-26 19:42:02 MST (UTC-7)
+#     REVISION:  2012-03-23 09:33:00 MST (UTC-7)
 #===============================================================================
 
 use strict;
@@ -23,6 +23,8 @@ use warnings;
 
 # Take the uptime
 my $uptime = `cat /proc/uptime | awk '{ print \$1 }'`;
+### For BSD (tested on OpenBSD 5.0, it also works on Linux)
+#my $uptime = `uptime | awk '{ split(\$3,a,":"); print (a[1]*60+a[2])*60 }'`
 
 # These help us calculate the minutes and hours
 my $MIN = 60;
